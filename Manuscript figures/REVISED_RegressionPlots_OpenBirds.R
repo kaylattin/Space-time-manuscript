110b29dbc0ce08bb8d0083d8e6011acc6190a816
Kayla Attinello <kaylaattinello@cmail.carleton.ca>
2022-12-22 16:29:19 -0500

    update

--- Manuscript figures/REVISED_RegressionPlots_OpenBirds.R
+++ Manuscript figures/REVISED_RegressionPlots_OpenBirds.R
@@ -11,13 +11,13 @@ gc()
 setwd("~/space-for-time")
 
 ### RICHNESS! ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-d <- read.csv("~/space-for-time/FinalDataset_RFsub.csv")
+d <- read.csv("~/space-for-time/FinalDataset_ROsub.csv")
 
 d$Region <- as.integer(as.factor(d$ref))
 d$Obs_ID <- as.integer(as.factor(d$ObsN))
 d$Cover_std <- as.vector(scale(d$Forest.cover))
 
-load("~/space-for-time/Output_RFsub_FINAL.RData")
+load("~/space-for-time/Output_ROsub_FINAL.RData")
 draws <- rstan::extract(stanfit, pars = c("a", "b_space", "b_time", "retrans_noise", "observer", "first" ))
 avg_space <- summary(stanfit, pars = "avg_b_space")
 avg_time <- summary(stanfit, pars = "avg_b_time")
@@ -38,9 +38,9 @@ avF <- avg_first$summary[,1]
 d_space <- d %>% filter(space.time == 2)
 d_time <- d %>% filter(space.time == 1)
 
-#png("REVISED_Slopes_RF.png",width=40,height=30,units="cm",res=600)
+#png("REVISED_Slopes_RO.png",width=40,height=30,units="cm",res=600)
 
-Cairo(file="REVISED_Slopes_RF_bg.png", 
+Cairo(file="REVISED_Slopes_RO_bg.png", 
       type="png",
       units="cm", 
       width=40, 
@@ -54,7 +54,7 @@ par(mfrow=c(1,2),
     mar=c(4,5,2,2))
 
 plot(d_space$Forest.cover, d_space$Richness, #d_space$TA,
-     col = alpha("#2c7bb6", 0), 
+     col = alpha("#253494", 0), 
      pch = 3,
      yaxt = "n", cex.axis = 1.5, cex.lab = 2, 
      cex.main = 2, main = "", xlab = "Percent forest cover", ylab = "Species richness")
@@ -78,14 +78,14 @@ for(i in 1:30){
   
   
   
-  shade( ci.time, x.scaled, col = "#25349403")
-  lines( x.scaled, mu.time, col = "#25349466", lwd = 1.5 )
+  shade( ci.time, x.scaled, col = "#41b6c403")
+  lines( x.scaled, mu.time, col = "#41b6c466", lwd = 1.5 )
 }
 
 
 
 plot(d_space$Forest.cover, d_space$Richness,
-     col = alpha("#ED432D", 0), 
+     col = alpha("#DC267F", 0), 
      pch = 3,
      yaxt = "n", cex.axis = 1.5, cex.lab = 2, 
      cex.main = 2, main = "", xlab = "Percent forest cover", ylab = "Species richness")
@@ -101,21 +101,20 @@ for(i in 1:30){
   
   x.scaled <- (x.seq-mean(d_space$Forest.cover))/sd(d_space$Forest.cover)
   mu.space <- sapply(x.scaled,  function(x) mean(exp(draws$a[, i, 1] + draws$b_space[, i] * x + 
-                                                      mean(draws$observer[, , 1]) + mean(draws$first[,1:2]) +
-                                                      draws$retrans_noise )) )
+                                                       mean(draws$observer[, , 1]) + mean(draws$first[,1:2]) +
+                                                       draws$retrans_noise )) )
   
   ci.space <- sapply(x.scaled,  function(x) PI(exp(draws$a[, i, 1] + draws$b_space[, i] * x + 
-                                                    mean(draws$observer[, , 1]) + mean(draws$first[,1:2]) +
-                                                    draws$retrans_noise )) )
+                                                     mean(draws$observer[, , 1]) + mean(draws$first[,1:2]) +
+                                                     draws$retrans_noise )) )
   
-  shade( ci.space, x.scaled, col = "#25349403")
-  lines( x.scaled, mu.space, col = "#25349466", lwd = 1.5 )
+  shade( ci.space, x.scaled, col = "#41b6c403")
+  lines( x.scaled, mu.space, col = "#41b6c466", lwd = 1.5 )
 }
 print("Finished SPACE plots! :-)")
 dev.off()
 
-
-png(file="REVISED_Slopes_RF_fg.png", 
+png(file="REVISED_Slopes_RO_fg.png", 
     type="cairo",
     units="cm", 
     width=40, 
@@ -128,7 +127,7 @@ par(mfrow=c(1,2),
     mar=c(4,5,2,2))
 
 plot(d_time$Forest.cover, d_time$Richness, 
-     col = alpha("#2c7bb6", 0), 
+     col = alpha("#41b6c4", 0), 
      pch = 3,
      yaxt = "n", cex.axis = 1.5, cex.lab = 2, 
      cex.main = 2, main = "", xlab = "Percent forest cover", ylab = "Species richness")
@@ -136,7 +135,7 @@ plot(d_time$Forest.cover, d_time$Richness,
 axis(2, cex.axis = 1.5)
 
 av_mu.time <- sapply(x.scaled, function(x) exp(avT_A + avT_B * x + avT_O + avF + mean(draws$retrans_noise)))
-lines(x.scaled, av_mu.time, col = "#253494", lwd = 5)
+lines(x.scaled, av_mu.time, col = "#41b6c4", lwd = 5)
 
 plot(d_space$Forest.cover, d_space$Richness, 
      col = alpha("#2c7bb6", 0), 
@@ -146,7 +145,7 @@ plot(d_space$Forest.cover, d_space$Richness,
 
 axis(2, cex.axis = 1.5)
 av_mu.space <- sapply(x.scaled, function(x) exp(avS_A + avS_B * x + avS_O + avF + mean(draws$retrans_noise)))
-lines(x.scaled, av_mu.space, col = "#253494", lwd = 5, lty = 2)
+lines(x.scaled, av_mu.space, col = "#41b6c4", lwd = 5, lty = 2)
 
 dev.off()
 
@@ -154,13 +153,13 @@ dev.off()
 rm(list = ls())
 gc()
 
-d <- read.csv("~/space-for-time/FinalDataset_TFsub.csv")
+d <- read.csv("~/space-for-time/FinalDataset_TOsub.csv")
 
 d$Region <- as.integer(as.factor(d$ref))
 d$Obs_ID <- as.integer(as.factor(d$ObsN))
 d$Cover_std <- as.vector(scale(d$Forest.cover))
 
-load("~/space-for-time/Output_TFsub_FINAL.RData")
+load("~/space-for-time/Output_TOsub_FINAL.RData")
 draws <- rstan::extract(stanfit, pars = c("a", "b_space", "b_time", "retrans_noise", "observer", "first" ))
 avg_space <- summary(stanfit, pars = "avg_b_space")
 avg_time <- summary(stanfit, pars = "avg_b_time")
@@ -181,7 +180,7 @@ avF <- avg_first$summary[,1]
 d_space <- d %>% filter(space.time == 2)
 d_time <- d %>% filter(space.time == 1)
 
-Cairo(file="REVISED_Slopes_TF_bg.png", 
+Cairo(file="REVISED_Slopes_TO_bg.png", 
       type="png",
       units="cm", 
       width=40, 
@@ -194,7 +193,7 @@ par(mfrow=c(1,2),
     mar=c(4,5,2,2))
 
 plot(d_time$Forest.cover, d_time$TA,
-     col = alpha("#2c7bb6", 0), 
+     col = alpha("#addd8e", 0), 
      #ylim = c(0, 60),
      pch = 3,
      yaxt = "n", cex.axis = 1.5, cex.lab = 2, 
@@ -219,13 +218,13 @@ for(i in 1:30){
   
   
   
-  shade( ci.time, x.scaled, col = "#31a35403")
-  lines( x.scaled, mu.time, col = "#31a35480", lwd = 1.5 )
+  shade( ci.time, x.scaled, col = "#addd8e03")
+  lines( x.scaled, mu.time, col = "#addd8e80", lwd = 1.5 )
 }
 
 
 plot(d_space$Forest.cover, d_space$TA,
-     col = alpha("#ED432D", 0), 
+     col = alpha("#addd8e", 0), 
      #ylim = c(0, 60),
      pch = 3,
      yaxt = "n", cex.axis = 1.5, cex.lab = 2, 
@@ -249,20 +248,20 @@ for(i in 1:30){
                                                      mean(draws$observer[, , 1]) + mean(draws$first[,1:2]) +
                                                      draws$retrans_noise )) )
   
-  shade( ci.space, x.scaled, col = "#31a35403")
-  lines( x.scaled, mu.space, col = "#31a35480", lwd = 1.5 )
+  shade( ci.space, x.scaled, col = "#addd8e03")
+  lines( x.scaled, mu.space, col = "#addd8e80", lwd = 1.5 )
 }
 
 print("Finished SPACE plots! :-)")
 dev.off()
 
-png(file="REVISED_Slopes_TF_fg.png", 
-      type="cairo",
-      units="cm", 
-      width=40, 
-      height=30, 
-      bg = "transparent",
-      res=600)
+png(file="REVISED_Slopes_TO_fg.png", 
+    type="cairo",
+    units="cm", 
+    width=40, 
+    height=30, 
+    bg = "transparent",
+    res=600)
 
 par(mfrow=c(1,2),
     omi=c(0.5, 0.5, 0.5, 0.5),
@@ -277,7 +276,7 @@ plot(d_time$Forest.cover, d_time$TA,
 axis(2, cex.axis = 1.5)
 
 av_mu.time <- sapply(x.scaled, function(x) exp(avT_A + avT_B * x + avT_O + avF + mean(draws$retrans_noise)))
-lines(x.scaled, av_mu.time, col = "#31a354", lwd = 5)
+lines(x.scaled, av_mu.time, col = "#addd8e", lwd = 5)
 
 plot(d_space$Forest.cover, d_space$TA,
      col = alpha("#2c7bb6", 0), 
@@ -287,6 +286,6 @@ plot(d_space$Forest.cover, d_space$TA,
 
 axis(2, cex.axis = 1.5)
 av_mu.space <- sapply(x.scaled, function(x) exp(avS_A + avS_B * x + avS_O + avF + mean(draws$retrans_noise)))
-lines(x.scaled, av_mu.space, col = "#31a354", lwd = 5, lty=2)
+lines(x.scaled, av_mu.space, col = "#addd8e", lwd = 5, lty = 2)
 
 dev.off()
